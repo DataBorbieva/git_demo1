81af14c38d3fc12454c5064f184b24aed9f9127f
DataBorbieva <databorbieva@gmail.com>
2022-05-31 14:20:26 -0500

    added class2

---
+++ src/Class2.java
@@ -0,0 +1,14 @@
+public class Class2 {
+    public static void main(String[] args) {
+        System.out.println("There is a test case");
+        System.out.println("There is a test case");
+        System.out.println("There is a test case");
+        System.out.println("There is a test case");
+        System.out.println("There is a test case");
+        System.out.println("There is a test case");
+        System.out.println("There is a test case");
+        System.out.println("There is a test case");
+        System.out.println("There is a test case");
+        System.out.println("There is a test case");
+    }
+}
